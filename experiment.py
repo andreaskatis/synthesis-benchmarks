34a8c251ea26045994281c3d483abb190960653e
tlghj <guoxx668@gmail.com>
2017-04-14 15:01:30 -0500

    remove fixpoint_only code

--- experiment.py
+++ experiment.py
@@ -16,6 +16,11 @@ SECOND_EXPERIMENTS_DIR = 'smaccm'
 SECOND_IMPLEMENT_DIR = 'smaccm/kind'
 SECOND_ANOTHER_IMPLEMENT_DIR = 'smaccm/fixpoint'
 
+EXTRA_EXPERIMENTS_DIR = "fixpoint_only"
+EXTRA_IMPLEMENT_DIR = 'fixpoint_only/kind'
+EXTRA_ANOTHER_IMPLEMENT_DIR = 'fixpoint_only/fixpoint'
+
+
 
 NestList_overhead = []
 NestList_size = [] 
@@ -300,66 +305,6 @@ def drawPerformance():
 
 
 
-def execute_fixpoint_only():
-    print("enter fixpoint_only")
-    lus_files = glob.glob("*.lus")
-    if len(lus_files) == 0:
-        print("No Lustre files found in fixpoint_only directory")
-        sys.exit(-1)
-
-   # lus_path = os.path.join(experiments_dir, lus_file)
-
-    for i, lus_file in enumerate(lus_files):
-        run_realizability(lus_file)
-        run_fixpoint(lus_file)
-
-    print("finish R and fixpoint")
-
-    impl_files = glob.glob("*_skolem.smt2")
-    if len(impl_files) == 0:
-        print("No implement files found in fixpoint_only directory")
-        sys.exit(-1)
-    #print("moving impl files")
-    for i, impl_file in enumerate(impl_files):
-        shutil.move(impl_file, "fixpoint")
-
-    #remove dummy smt2files
-    smt_files = glob.glob("*.smt2")
-    if (len(smt_files)!=0):
-        for i, smt_file in enumerate(smt_files):
-            os.remove(smt_file)
-
-    os.chdir("fixpoint")
-    impl_files = glob.glob("*_skolem.smt2")
-    if len(impl_files) == 0:
-        print("No Skolem files found in fixpoint directory")
-        sys.exit(-1)
-
-
-    for i, impl_file in enumerate(impl_files):
-        args = ['java', '-jar', smtlib2c_jar,
-            '-iter', '1000000',
-            '-c_harness', '-lustrec_harness', '-lustrecnode', 'top', impl_file]
-
-        with open("debug_smtlib2c.txt", "a") as debug:
-            debug.write("Running SMTLib2C with arguments: {}\n".format(args))
-            proc = subprocess.Popen(args, stdout=debug)
-            proc.wait()
-            debug.write("\n")
-
-    print("finish smtlib2c")
-    
-    
-    for i, lus_file in enumerate(lus_files):
-        run_makefile(os.path.splitext(lus_file)[0])
-
-    print("finish run_makefile")
-
-    for i, lus_file in enumerate(lus_files):
-        run_executables(os.path.splitext(lus_file)[0])
-
-    print("finish run_executable")
-
 
 
 ################################################################################################
@@ -605,88 +550,84 @@ def execute(experiments_dir, push_path, another_push_path, implement_dir, anothe
 #################################################################################################
 
 
-jkind_jar = None
-path = os.environ.get("JKIND_HOME") or os.environ.get("PATH") or os.environ.get("path")
+# jkind_jar = None
+# path = os.environ.get("JKIND_HOME") or os.environ.get("PATH") or os.environ.get("path")
 
-for dir in path.split(':'):
-    jar = os.path.join(dir, "jkind.jar")
-    if os.path.exists(jar):
-        jkind_jar = jar
-        break
-if jkind_jar is None:
-    print("Unable to find jkind.jar in JKIND_HOME or PATH environment variables")
-    sys.exit(-1)
-print("Using JKind: " + jkind_jar)
-
-
-smtlib2c_jar = None
-path = os.environ.get("PATH") or os.environ.get("path")
-
-for dir in path.split(':'):
-    jar = os.path.join(dir, "SMTLib2C.jar")
-    if os.path.exists(jar):
-        smtlib2c_jar = jar
-        break
-if smtlib2c_jar is None:
-    print("Unable to find SMTLib2C.jar in PATH environment variables")
-    sys.exit(-1)
-
-print("Using SMTLib2C: " + smtlib2c_jar)
+# for dir in path.split(':'):
+#     jar = os.path.join(dir, "jkind.jar")
+#     if os.path.exists(jar):
+#         jkind_jar = jar
+#         break
+# if jkind_jar is None:
+#     print("Unable to find jkind.jar in JKIND_HOME or PATH environment variables")
+#     sys.exit(-1)
+# print("Using JKind: " + jkind_jar)
 
 
-##############################################################
-execute(EXPERIMENTS_DIR, PUSH_PATH, ANOTHER_PUSH_PATH, IMPLEMENT_DIR, ANOTHER_IMPLEMENT_DIR, NestList_size_name_verification)
-execute(SECOND_EXPERIMENTS_DIR, PUSH_PATH, ANOTHER_PUSH_PATH, SECOND_IMPLEMENT_DIR,SECOND_ANOTHER_IMPLEMENT_DIR, NestList_size_name_smaccm)
+# smtlib2c_jar = None
+# path = os.environ.get("PATH") or os.environ.get("path")
+
+# for dir in path.split(':'):
+#     jar = os.path.join(dir, "SMTLib2C.jar")
+#     if os.path.exists(jar):
+#         smtlib2c_jar = jar
+#         break
+# if smtlib2c_jar is None:
+#     print("Unable to find SMTLib2C.jar in PATH environment variables")
+#     sys.exit(-1)
+
+# print("Using SMTLib2C: " + smtlib2c_jar)
 
 
-#fill the NestList_overhead
-parse("debug_jkind.txt", "overhead.txt")
-writeOverhead(NestList_overhead, "overhead.txt")
-drawOverhead()
-print("NestList_overhead")
-print(NestList_overhead)
-
-print()
-
-print("NestList_size_name_verification")
-print(NestList_size_name_verification)
-print("NestList_size_name_smaccm")
-print(NestList_size_name_smaccm)
+# ##############################################################
+# execute(EXPERIMENTS_DIR, PUSH_PATH, ANOTHER_PUSH_PATH, IMPLEMENT_DIR, ANOTHER_IMPLEMENT_DIR, NestList_size_name_verification)
+# execute(SECOND_EXPERIMENTS_DIR, PUSH_PATH, ANOTHER_PUSH_PATH, SECOND_IMPLEMENT_DIR,SECOND_ANOTHER_IMPLEMENT_DIR, NestList_size_name_smaccm)
 
 
-print("current path =" + os.getcwd())
+# #fill the NestList_overhead
+# parse("debug_jkind.txt", "overhead.txt")
+# writeOverhead(NestList_overhead, "overhead.txt")
+# drawOverhead()
+# print("NestList_overhead")
+# print(NestList_overhead)
 
-#create both verification (kind and fixpoint) loc.txt 
-measureSizeOfC(IMPLEMENT_DIR, NestList_size_name_verification)
-measureSizeOfC(ANOTHER_IMPLEMENT_DIR, NestList_size_name_verification)
-#create both smaccm (kind and fixpoint) loc.txt
-measureSizeOfC(SECOND_IMPLEMENT_DIR, NestList_size_name_smaccm)
-measureSizeOfC(SECOND_ANOTHER_IMPLEMENT_DIR, NestList_size_name_smaccm)
+# print()
 
-#append to NestList_size
-combineSizeTxt(IMPLEMENT_DIR+"/loc.txt", ANOTHER_IMPLEMENT_DIR+"/loc.txt")
-combineSizeTxt(SECOND_IMPLEMENT_DIR+"/loc.txt", SECOND_ANOTHER_IMPLEMENT_DIR+"/loc.txt")
-
-print("NestList_size")
-print(NestList_size)
-
-drawSize()
-print()
-
-#append to NestList_performance
-combineResultTxt(IMPLEMENT_DIR+"/results.txt",ANOTHER_IMPLEMENT_DIR+"/results.txt")
-combineResultTxt(SECOND_IMPLEMENT_DIR+"/results.txt",SECOND_ANOTHER_IMPLEMENT_DIR+"/results.txt")
-
-print("NestList_performance")
-print(NestList_performance)
-
-drawPerformance()
+# #print("NestList_size_name_verification")
+# #print(NestList_size_name_verification)
+# #print("NestList_size_name_smaccm")
+# #print(NestList_size_name_smaccm)
 
 
+# print("current path =" + os.getcwd())
 
-#run fixpoint_only 
-os.chdir("fixpoint_only")
-#execute_fixpoint_only()
+# #create both verification (kind and fixpoint) loc.txt 
+# measureSizeOfC(IMPLEMENT_DIR, NestList_size_name_verification)
+# measureSizeOfC(ANOTHER_IMPLEMENT_DIR, NestList_size_name_verification)
+# #create both smaccm (kind and fixpoint) loc.txt
+# measureSizeOfC(SECOND_IMPLEMENT_DIR, NestList_size_name_smaccm)
+# measureSizeOfC(SECOND_ANOTHER_IMPLEMENT_DIR, NestList_size_name_smaccm)
+
+# #append to NestList_size
+# combineSizeTxt(IMPLEMENT_DIR+"/loc.txt", ANOTHER_IMPLEMENT_DIR+"/loc.txt")
+# combineSizeTxt(SECOND_IMPLEMENT_DIR+"/loc.txt", SECOND_ANOTHER_IMPLEMENT_DIR+"/loc.txt")
+
+# print("NestList_size")
+# print(NestList_size)
+
+# drawSize()
+# print()
+
+# #append to NestList_performance
+# combineResultTxt(IMPLEMENT_DIR+"/results.txt",ANOTHER_IMPLEMENT_DIR+"/results.txt")
+# combineResultTxt(SECOND_IMPLEMENT_DIR+"/results.txt",SECOND_ANOTHER_IMPLEMENT_DIR+"/results.txt")
+
+# print("NestList_performance")
+# print(NestList_performance)
+
+# drawPerformance()
+
+
 
 
 
